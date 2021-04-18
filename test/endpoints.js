fce83b470c09a2aec706d87b291f43e8787e3668
inuchamal <inuchamal@outlook.com>
2021-04-19 01:29:20 +0800

    test: add api/targets post request test

--- test/endpoints.js
+++ test/endpoints.js
@@ -2,6 +2,9 @@ process.env.NODE_ENV = 'test'
 
 var test = require('ava')
 var servertest = require('servertest')
+var fs = require('fs')
+var path = require('path')
+var bl = require('bl')
 
 var server = require('../lib/server')
 
@@ -14,4 +17,23 @@ test.serial.cb('healthcheck', function (t) {
     t.is(res.body.status, 'OK', 'status is ok')
     t.end()
   })
+})
+
+test.serial.cb('Post api/targets', function (t) {
+  var url = '/api/targets'
+  // servertest is a duplex stream when posting data
+  var serverStream = servertest(server(), url, { method: 'POST', headers: { ...{ 'content-type': 'application/json' } } })
+
+  // pipe data to the POST request
+  fs.createReadStream(path.join(__dirname, 'post.json')).pipe(serverStream)
+
+  // pipe data from the response
+  serverStream.pipe(bl(function (err, res) {
+    res = JSON.parse(res)
+    console.log(res)
+    t.falsy(err, 'no error')
+
+    t.is(res.status, 'OK', 'status is ok')
+    t.end()
+  }))
 })
